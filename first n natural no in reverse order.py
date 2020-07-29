f62717697e2c95fd5d43f0657429b0fc2c838dda
shubhangimohije <58595887+shubhangimohije@users.noreply.github.com>
2020-07-29 19:57:03 +0530

    Add files via upload

---
+++ first n natural no in reverse order.py
@@ -0,0 +1,14 @@
+#WAP to print first n numbers in reverse order by using while
+
+
+i=10
+while i>=1:
+    print(i,end=' ')
+    i-=1
+
+
+
+#by using range
+
+for x in range(10,0,-1):
+    print(x,end=' ')
